fed7bc857e305e2c31c8136555d1bff28076352d
bruno78 <devbruno78@gmail.com>
2018-05-13 18:54:47 -0400

    add detail layout

--- app/src/main/java/com/brunogtavares/popmovies/MovieDetailActivity.java
+++ app/src/main/java/com/brunogtavares/popmovies/MovieDetailActivity.java
@@ -1,11 +1,7 @@
 package com.brunogtavares.popmovies;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class MovieDetailActivity extends AppCompatActivity {
 
@@ -13,16 +9,5 @@ public class MovieDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
