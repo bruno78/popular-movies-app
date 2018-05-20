5886141fbfafe94b9af84c5a9265ce9242df5186
bruno78 <devbruno78@gmail.com>
2018-05-19 23:58:45 -0400

    Clean a bit

--- app/src/main/java/com/brunogtavares/popmovies/MoviesActivity.java
+++ app/src/main/java/com/brunogtavares/popmovies/MoviesActivity.java
@@ -135,7 +135,6 @@ public class MoviesActivity extends AppCompatActivity
         if(!movies.isEmpty() && movies != null) {
             Log.i(LOG_TAG, String.valueOf(movies.size()));
             mMovieAdapter = new MovieAdapter(this, movies);
-            mMovieAdapter.notifyDataSetChanged();
         }
 
     }
